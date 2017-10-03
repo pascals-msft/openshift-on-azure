8a6a9109eb1f09622ca300e32b075c0266ca9998
Pascal Sauliere <pascals@microsoft.com>
2017-10-03 22:10:39 +0200

    Comments in json_value.py

--- json_value.py
+++ json_value.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python
 #
-# json_value.py
-#
-# given a JSON object and a name, returns the value associated with the name
+# Given a JSON object and a name, returns the value associated with the name
 # (no error checking)
-
+# Example:
+#   $ ./json_value.py '{"a":"value_a","b":"value_b"}' "b"
+#   value_b
+#   $
 import sys, json
-
 print(json.loads(sys.argv[1])[sys.argv[2]])
-
-# print(sys.argv)
-# print(len(sys.argv))
-# for i in range(len(sys.argv)):
-#     print i, sys.argv[i]
-
-# parsed_json = json.loads(sys.argv[1])
-# print(parsed_json[sys.argv[2]])
